7ecfacc8c9f0ee10d82f84407f77b2ba9c5ab2ac
Fran Guijarro <franleplant@gmail.com>
2021-09-11 18:55:40 -0300

    initial setup

---
+++ src/pages/api/nfts/[collection]/[id].ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+interface INFTAttr {
+  display_type: string;
+  trait_type: string;
+  value: number | string;
+}
+
+interface INFTMetadata {
+  description: string;
+  external_url: string;
+  image: string;
+  name: string;
+  attributes: Array<Partial<INFTAttr>>;
+  background_color: string;
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Partial<INFTMetadata>>
+) {
+  res.status(200).json({
+    description: "CuCo description",
+    external_url: req.url,
+    image:
+      "https://storage.googleapis.com/opensea-prod.appspot.com/puffs/3.png",
+    name: "CuCo test",
+    attributes: [
+      {
+        trait_type: "Kind",
+        value: "Test",
+      },
+    ],
+  });
+}
